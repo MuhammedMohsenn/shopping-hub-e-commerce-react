1f48320d8232bdbe11645804a0a9195ebe68378b
Mohamed Mohsen <muhammedmohsen8695@gmail.com>
2023-10-05 00:49:24 +0300

    fixed error with api

--- src/components/SingleCategory/SingleCategory.js
+++ src/components/SingleCategory/SingleCategory.js
@@ -24,37 +24,41 @@ const SingleCategory = ({ products, status }) => {
       {isModalVisible && <SingleProduct />}
 
       <div className="container">
-        <div className="cat-single-content">
-          <div className="section-title">
-            <h3 className="text-uppercase fw-7 text-regal-blue ls-1">
-              {products[0].category.name}
-            </h3>
-          </div>
-          <div className="product-items grid">
-            {products.map((product) => (
-              <div
-                className="product-item bg-white"
-                key={product.id}
-                onClick={() => viewModalHandler(product)}
-              >
-                <div className="product-item-img">
-                  <img src={product.images[0]} alt="" />
-                  <div className="product-item-cat text-white fs-13 text-uppercase bg-gold fw-6">
-                    {product.category.name}
+        {products ? (
+          <div className="cat-single-content">
+            <div className="section-title">
+              <h3 className="text-uppercase fw-7 text-regal-blue ls-1">
+                {products[0].category.name}
+              </h3>
+            </div>
+            <div className="product-items grid">
+              {products.map((product) => (
+                <div
+                  className="product-item bg-white"
+                  key={product.id}
+                  onClick={() => viewModalHandler(product)}
+                >
+                  <div className="product-item-img">
+                    <img src={product.images[0]} alt="" />
+                    <div className="product-item-cat text-white fs-13 text-uppercase bg-gold fw-6">
+                      {product.category.name}
+                    </div>
+                  </div>
+                  <div className="product-item-body">
+                    <h6 className="product-item-title text-pine-green fw-4 fs-15">
+                      {product.title}
+                    </h6>
+                    <div className="product-item-price text-regal-blue fw-7 fs-18">
+                      {formatPrice(product.price)}
+                    </div>
                   </div>
                 </div>
-                <div className="product-item-body">
-                  <h6 className="product-item-title text-pine-green fw-4 fs-15">
-                    {product.title}
-                  </h6>
-                  <div className="product-item-price text-regal-blue fw-7 fs-18">
-                    {formatPrice(product.price)}
-                  </div>
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
+        ) : (
+          <Error />
+        )}
       </div>
     </section>
   );
