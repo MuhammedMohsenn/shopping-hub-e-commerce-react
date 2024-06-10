f8f4081b91e5b71cdd1896a5b41587d9146c0973
MuhammedMohsenn <muhammedmohsen8695@gmail.com>
2024-06-10 11:55:32 +0300

    search logic and some edits and changes

---
+++ src/components/Search/Search.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+// import "./Search.scss";
+
+const Search = () => {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (query.trim()) {
+      navigate(`/search?query=${query}`);
+    }
+  };
+
+  return (
+    <form className="navbar-search flex" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search here ..."
+        value={query}
+        onChange={handleInputChange}
+      />
+      <button type="submit" className="navbar-search-btn">
+        <i className="fas fa-search"></i>
+      </button>
+    </form>
+  );
+};
+
+export default Search;
---
+++ src/components/Search/SearchResults.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { formatPrice } from "../../utils/helpers";
+import { useDispatch, useSelector } from "react-redux";
+import { setIsModalVisible, setModalData } from "../../store/modalSlice";
+import "../ProductList/ProductList.scss";
+import SingleProduct from "../SingleProduct/SingleProduct";
+import Error from "../Error/Error";
+import Loader from "../Loader/Loader";
+
+const SearchResults = () => {
+  const dispatch = useDispatch();
+  const { isModalVisible } = useSelector((state) => state.modal);
+  const location = useLocation();
+  const query = new URLSearchParams(location.search).get("query");
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchAllProducts = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          "https://api.escuelajs.co/api/v1/products"
+        );
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        const filteredResults = data.filter((product) =>
+          product.title.toLowerCase().includes(query.toLowerCase())
+        );
+
+        setResults(filteredResults);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        setError("Error fetching products");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (query) {
+      fetchAllProducts();
+    }
+  }, [query]);
+
+  const viewModalHandler = (data) => {
+    dispatch(setModalData(data));
+    dispatch(setIsModalVisible(true));
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
+
+  return (
+    <div className="search-results py-5 bg-ghost-white">
+      <div className="container">
+        <div className="section-title">
+          <h3 className="text-uppercase fw-7 text-regal-blue ls-1">
+            Search Results for "{query}"
+          </h3>
+        </div>
+        {isModalVisible && <SingleProduct />}
+        {results.length > 0 ? (
+          results.map((product) => (
+            <div className="product-items grid">
+              <div
+                className="product-item bg-white"
+                key={product.id}
+                onClick={() => viewModalHandler(product)}
+              >
+                <div className="product-item-img">
+                  <img src={product.images[0]} alt="" />
+                  <div className="product-item-cat text-white fs-13 text-uppercase bg-gold fw-6">
+                    {product.category.name}
+                  </div>
+                </div>
+                <div className="product-item-body">
+                  <h6 className="product-item-title text-pine-green fw-4 fs-15">
+                    {product.title}
+                  </h6>
+                  <div className="product-item-price text-regal-blue fw-7 fs-18">
+                    {formatPrice(product.price)}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className="section-title">
+            <h3 className="text-uppercase fw-5 text-regal-blue ls-1">
+              No Results found for "{query}"
+            </h3>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SearchResults;
